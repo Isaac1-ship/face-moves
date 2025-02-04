0e2595b1df2440a2886d8712e07203c254c5a065
Isaac1-ship <28tgilman@aos92.org>
2025-02-04 10:09:16 -0500

    walking and handshake

--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
-
+controller.A.onEvent(ControllerButtonEvent.Repeated, function () {
+    mySprite = sprites.create(assets.image`stick man`, SpriteKind.Player)
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`hand shake`,
+    200,
+    true
+    )
+})
+let mySprite: Sprite = null
+let mySprite2 = sprites.create(assets.image`stick man`, SpriteKind.Player)
+animation.runImageAnimation(
+mySprite,
+assets.animation`myAnim`,
+200,
+true
+)
